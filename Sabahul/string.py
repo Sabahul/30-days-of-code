94696917c28a066042d9c84b8a97b522e8ddb163
SABAHUL MUNZARIN <sabahulmunzarin19@gmail.com>
2022-04-26 08:31:51 +0530

    Update string.py

--- Sabahul/string.py
+++ Sabahul/string.py
@@ -1,5 +1,6 @@
-#the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string
-# String traversal will take place from left to right, not from right to left
+'''the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string
+ String traversal will take place from left to right, not from right to left'''
+
 def count_substring(string, sub_string):
     count=0
     for i in range(len(string)):
