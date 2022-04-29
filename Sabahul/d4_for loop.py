9de614a48f5411c5c3f32dd4ee546369994c0938
SABAHUL MUNZARIN <sabahulmunzarin19@gmail.com>
2022-04-29 20:11:05 +0530

    Create d4_for loop.py

---
+++ Sabahul/d4_for loop.py
@@ -0,0 +1,12 @@
+# Program to find the sum of all numbers stored in a list
+
+
+numbers = [63, 51, 32, 8, 42, 20, 11]
+sum = 0
+for val in numbers:
+    sum = sum+val
+
+print("The sum is", sum)
+
+### output..
+The sum is 227
