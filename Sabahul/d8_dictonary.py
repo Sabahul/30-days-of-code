9b0682026c9ab1c4e34f27e37f97e709bc80548e
SABAHUL MUNZARIN <sabahulmunzarin19@gmail.com>
2022-05-02 00:05:45 +0530

    Update d8_dictonary.py

--- Sabahul/d8_dictonary.py
+++ Sabahul/d8_dictonary.py
@@ -5,12 +5,12 @@ print(Dict)
 
 # Creating a Dictionary
 # with dict() method
-Dict = dict({1: 'Geeks', 2: 'For', 3:'Geeks'})
+Dict = dict({1: 'Welcome', 2: 'to', 3:'Chandigarh' 4:'university'})
 print("\nDictionary with the use of dict(): ")
 print(Dict)
 
 # Creating a Dictionary
 # with each item as a Pair
-Dict = dict([(1, 'Geeks'), (2, 'For')])
+Dict = dict([(1, 'Welcome'), (2, 'to')])
 print("\nDictionary with each item as a pair: ")
 print(Dict)
