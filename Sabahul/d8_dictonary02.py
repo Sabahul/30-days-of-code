a7a3180901d067f8e7660d72b1d5724dd287269c
SABAHUL MUNZARIN <sabahulmunzarin19@gmail.com>
2022-05-02 00:16:12 +0530

    Create d8_dictonary02.py

---
+++ Sabahul/d8_dictonary02.py
@@ -0,0 +1,11 @@
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'Sabahul', 2: 'Welcome', 3: 'To', 4:'CU'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'Sabahul', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
