221081edf7b461bfeec67e0c3af6df3c18c6b008
SABAHUL MUNZARIN <sabahulmunzarin19@gmail.com>
2022-05-03 20:43:48 +0530

    Create d9_Array2.py

---
+++ Sabahul/d9_Array2.py
@@ -0,0 +1,14 @@
+
+
+## 1D array
+print("How many element to store in the list ? ", end="")
+n = input()
+arr = []
+print("\nEnter", n, "Elements: ", end="")
+n = int(n)
+for i in range(n):
+    element = input()
+    arr.append(element)
+print("\nThe list is:")
+for i in range(n):
+    print(arr[i], end=" ")
