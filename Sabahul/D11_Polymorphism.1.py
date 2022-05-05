1159c0b9d031f5b5f48179f739518355e310a8f8
SABAHUL MUNZARIN <sabahulmunzarin19@gmail.com>
2022-05-06 00:07:40 +0530

    Create D11_Polymorphism.1.py

---
+++ Sabahul/D11_Polymorphism.1.py
@@ -0,0 +1,26 @@
+class Bihar():
+	def capital(self):
+		print("Patna is the capital of Bihar.")
+
+	def language(self):
+		print("Bhojpuri and Hindi is the most widely spoken language of Bihar.")
+
+	def type(self):
+		print("Bihar is a developing state.")
+
+class Punjab():
+	def capital(self):
+		print("Chandigarh is the capital of Punjab.")
+
+	def language(self):
+		print("Punjabi is the primary language of USA.")
+
+	def type(self):
+		print("Punjab is also a developing state.")
+
+obj_bih = Bihar()
+obj_ = Punjab()
+for country in (obj_Bih, obj_Pun):
+	state.capital()
+	state.language()
+	state.type()
