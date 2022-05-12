4f388052b439bd1b2ea90c6e626ab742598da450
SABAHUL MUNZARIN <sabahulmunzarin19@gmail.com>
2022-05-12 15:07:41 +0530

    Create D18_String creating.py

---
+++ Sabahul/D18_String creating.py
@@ -0,0 +1,25 @@
+# Creating a String
+# with single Quotes
+String1 = 'Welcome to the chandigarh university'
+print("String with the use of Single Quotes: ")
+print(String1)
+
+# Creating a String
+# with double Quotes
+String1 = "I'm a Sabahul"
+print("\nString with the use of Double Quotes: ")
+print(String1)
+
+# Creating a String
+# with triple Quotes
+String1 = '''I'm a Sabahul and I am frrom Chandigarh university'''
+print("\nString with the use of Triple Quotes: ")
+print(String1)
+
+# Creating String with triple
+# Quotes allows multiple lines
+String1 = '''life
+			is all about your
+			choice'''
+print("\nCreating a multiline String: ")
+print(String1)
