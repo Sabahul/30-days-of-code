695fea4b4b3dc77f73ba25041b5c245a1f2dd328
SABAHUL MUNZARIN <sabahulmunzarin19@gmail.com>
2022-05-22 21:23:25 +0530

    Create D28_Pop.py

---
+++ Sabahul/D28_Pop.py
@@ -0,0 +1,20 @@
+# programming languages list
+languages = ['Python', 'Java', 'C++', 'Ruby', 'C']
+
+# remove and return the last item
+print('When index is not passed:') 
+print('Return Value:', languages.pop())
+
+print('Updated List:', languages)
+
+# remove and return the last item
+print('\nWhen -1 is passed:') 
+print('Return Value:', languages.pop(-1))
+
+print('Updated List:', languages)
+
+# remove and return the third last item
+print('\nWhen -3 is passed:') 
+print('Return Value:', languages.pop(-3))
+
+print('Updated List:', languages)
